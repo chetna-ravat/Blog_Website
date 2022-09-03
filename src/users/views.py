d6bed749f4a4299d552e8a02760ef876b1216082
Chetna Ravat <chetnaravat23@gmail.com>
2022-09-02 23:53:24 -0400

    Final user registration page

--- src/users/views.py
+++ src/users/views.py
@@ -1,13 +1,16 @@
+
+
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
+
 
 
 # Create your views here.
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
@@ -15,7 +18,7 @@ def register(request):
         else:
             messages.error(request,f'User account cannot be created due to invalid inputs')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html',{'form': form})
 
 
