6f90965ade03be2a90cd66130a906ebc41339963
Chetna Ravat <chetnaravat23@gmail.com>
2022-09-10 18:25:39 -0400

    1) Adding pagination to the blog app 2) Filtering blog posts by user name

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.template import context
 from django.views.generic import (
     ListView, 
@@ -27,14 +28,38 @@ class PostListView(ListView):
     context_object_name = 'posts' 
     # date_posted order the list from oldest to newest, to reverse we add a - sign in the front
     ordering = ['-date_posted']
+    paginate_by = 5
+    
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
 
 
 class PostDetailView(DetailView):
     model = Post
     fields = ["title", "content"]
+    # When we are not defining template_name = <FILENAME>.html in our django defined class function, this means 
+    # that django will search for a template with default naming convention of # <app>/<model>_<viewtype>.html 
+    # In our PostDetailView function, it is looking for blog/post_detail.html
+    # We had passed a template in PostListView since we had already created a template with another name and we
+    # wanted django to search for it ,not the file with filename constructed using default naming convention.
     
      
 class PostCreateView(LoginRequiredMixin, CreateView):
+    # When we are not defining template_name = <FILENAME>.html in our django defined class function, this means 
+    # that django will search for a template with default naming convention of # <app>/<model>_form.html 
+    # In our PostCreateView & PostUpdateView function, it is looking for blog/post_form.html
+    
+    
     model = Post
     fields = ['title','content']
 
