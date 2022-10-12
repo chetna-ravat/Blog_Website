e272396384e3963dfc6441ccb5c17f6145d25d46
Chetna Ravat <chetnaravat23@gmail.com>
2022-10-11 22:41:49 -0400

    Add email support

--- src/django_project/settings.py
+++ src/django_project/settings.py
@@ -151,3 +151,11 @@ LOGIN_REDIRECT_URL = 'blog-home'
 # eg: I cannot access my profile without loggin in, so when I login then django would take me to accounts page 
 # This would be so frustrating, so we change the route to login
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+RECIPIENT_ADDRESS = env('RECIPIENT_ADDRESS')
